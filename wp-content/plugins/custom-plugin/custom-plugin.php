7b0067a1a56dc85ad8097cc0b3f492a36dbc76ba
Studio Creatorio <wpcreatorio@gmail.com>
2023-08-23 22:15:37 +0700

    Mengganti nama index.php menjadi custom-plugin.php

---
+++ wp-content/plugins/custom-plugin/custom-plugin.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Baheunk Custom Plugin
+ *
+ * @package       BCP
+ * @author        Baheunk
+ *
+ * @wordpress-plugin
+ * Plugin Name:   Baheunk Custom Plugin
+ * Plugin URI:    https://www.riomastri.com
+ * Description:   Sebuah percobaan untuk membuat plugin dengan fungsi yang belum pasti :D
+ * Version:       1.0.0
+ * Author:        Baheunk
+ * Author URI:    https://riomastri.com
+ * Update URI:    https://www.riomastri.com
+ * Text Domain:   BCP
+ * Domain Path:   /lang
+ */
+
+?>
